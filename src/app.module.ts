846182df9ba146d3106def48efbec2c0416b29e7
Adonis Tividad <adonis@spls.ae>
2023-07-13 22:51:43 +0400

    added typeorm mongose  support

--- src/app.module.ts
+++ src/app.module.ts
@@ -13,31 +13,41 @@ import { UsersModule } from './modules/users/users.module';
 import { PelpDataMastersModule } from './modules/pelp_data_masters/pelp_data_masters.module';
 import { PelpDataMasterDetailsModule } from './modules/pelp_data_master_details/pelp_data_master_details.module';
 import { ConfigModule, ConfigService } from '@nestjs/config';
+import { join } from 'path';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
       // envFilePath: ['.env.development','.env.development.local', '.env.development'],
-      envFilePath: ['.env.aws'],
-      // envFilePath: ['.env'],
+      // envFilePath: ['.env.aws'],
+      // envFilePath: ['.env.local'],
+      envFilePath: ['.env'],
     }),
-    TypeOrmModule.forRootAsync({
-      imports: [ConfigModule],      
-      useFactory: (configService: ConfigService) => ({
-        type: 'mysql',
-        host: configService.get<string>('DATABASE_HOST'),
-        port: parseInt(configService.get<string>('DATABASE_PORT')),
-        database: configService.get<string>('DATABASE_NAME'),
-        username: configService.get<string>('DATABASE_USERNAME'),
-        password: configService.get<string>('DATABASE_PASSWORD'),
-        // sche: configService.get<string>('DATABASE_PASSWORD'),
-        entities: [__dirname + '/**/*.entity{.ts,.js}'],
-        // logging: true,
-        // synchronize: false,
-        synchronize: true, //development only
-      }),
-      inject: [ConfigService],
+    // TypeOrmModule.forRootAsync({
+    //   imports: [ConfigModule],
+    //   useFactory: (configService: ConfigService) => ({
+    //     type: 'mysql',
+    //     host: configService.get<string>('DATABASE_HOST'),
+    //     port: parseInt(configService.get<string>('DATABASE_PORT')),
+    //     database: configService.get<string>('DATABASE_NAME'),
+    //     username: configService.get<string>('DATABASE_USERNAME'),
+    //     password: configService.get<string>('DATABASE_PASSWORD'),
+    //     // sche: configService.get<string>('DATABASE_PASSWORD'),
+    //     entities: [__dirname + '/**/*.entity{.ts,.js}'],
+    //     // logging: true,
+    //     // synchronize: false,
+    //     synchronize: true, //development only
+    //   }),
+    //   inject: [ConfigService],
+    // }),
+    TypeOrmModule.forRoot({
+      type: 'mongodb',
+      url: 'mongodb+srv://mongo:mongo@cluster0.zlhy3yy.mongodb.net/mars_mysql?retryWrites=true&w=majority',
+      entities: [join(__dirname, '**/**.entity{.ts,.js}')],
+      synchronize: true,
+      useNewUrlParser: true,
+      logging: true,
     }),
     UsersModule,
     ProductsModule,
